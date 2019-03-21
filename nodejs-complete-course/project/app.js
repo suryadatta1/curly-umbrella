0d98643fe5eca14b7049aca2fbb1120c53849e4b
suryadatta <suryadattatangirala@outlook.com>
2019-03-21 21:13:25 +0530

    add cart functionality

--- nodejs-complete-course/project/app.js
+++ nodejs-complete-course/project/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,13 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5c939dfb53e54844b07d762')
-     .then(user => {
-       req.user = user;
-       next();
-     })
-     .catch(err => console.log(err));
-  next();
+  User.findById('5c939dfb53e54844b07d7625')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
--- nodejs-complete-course/project/models/user.js
+++ nodejs-complete-course/project/models/user.js
@@ -1,21 +1,62 @@
-const mongodb = require('mongodb')
-const getdb = require('../util/database').getDb;
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const ObjectId = mongodb.ObjectID;
+const ObjectId = mongodb.ObjectId;
 
 class User {
-constructor(username,email) {
-  this.name = username;
-  this.email = email;
+  constructor(username, email, cart, id) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems
+    };
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection('users')
+      .findOne({ _id: new ObjectId(userId) })
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
 
-save() {
-  const db = getdb();
-  return db.collection('users').insertOne(this);
-}
- static findById(userId) {
-  const db =getdb();
-  return db.collection('user').findOne({_id:new ObjectId(userId)})
- }
-}
 module.exports = User;
